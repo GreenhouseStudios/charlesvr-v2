ce7f3873d15ed6c6d232cd63e9006e34f0eee117
melinaskye <melinaskye@optonline.net>
2022-10-05 11:17:14 -0400

    remove chapter button

---
+++ src/components/next-chapter.svelte
@@ -0,0 +1,16 @@
+<!-- <script>
+	export let light = false;
+	export let classes = '';
+	export { classes as class };
+	export let flex = 'flex';
+</script>
+
+<div>
+	<p class='m-10 p-3 bg-white  bg-opacity-90 shadow-lg text-center'> Continue to next Chapter </p>
+</div>
+
+<style>
+	.p-4 {
+		padding: clamp(16px, 1em, 32px);
+	}
+</style> -->
