89612a29a98b08a833147c5824caccca7d6c530e
melinaskye <melinaskye@optonline.net>
2022-10-26 11:50:24 -0400

    making chapter bustton responsive

--- src/components/chapter-button.svelte
+++ src/components/chapter-button.svelte
@@ -8,18 +8,16 @@
 	export let light = false;
 </script>
 
-<div class=" w-full sm:w-max">
-	<NextChapter>
-	</NextChapter>
+<div class=" sm:w-max m-5">
 	<a
 		id="button"
-		class="group flex m-10 flex-col items-center rounded-xl px-6 shadow-lg shadow-black/30 transition-all duration-500 sm:flex-row sm:gap-6"
+		class="group flex flex-row items-center justify-center rounded-xl px-6 shadow-lg shadow-black/30 transition-all duration-500  sm:gap-6"
 		href={path}
 	>
 		<p class="pb-2  px-2 text-6xl text-gold-500 transition-colors duration-500 group-hover:text-gold-400">
 			{numeral}
 		</p>
-		<p class="py-6 text-3xl text-white px-5">
+		<p class="py-6 text-3xl text-white px-5 chapter">
 			{chapter} 	
 		</p>
 		<ChevronRightIcon size="3x" class="text-gold-500 transition-colors duration-1000 group-hover:text-gold-400"/>
@@ -36,4 +34,16 @@
 	#button:hover {
 		background-position: left bottom;
 	}
+
+	@media screen and (max-width: 650px) {
+  .chapter {
+    display: none;
+  }
+  #button {
+	display:flex;
+	flex-direction: row;
+	justify-content: center;
+	width: max-content;
+  }
+}
 </style>
