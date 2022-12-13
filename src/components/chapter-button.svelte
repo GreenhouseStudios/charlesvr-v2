e296867246531cbe09edd1f506355be4a702930f
melinaskye <melinaskye@optonline.net>
2022-12-13 15:36:23 -0500

    margins of the chapter button

--- src/components/chapter-button.svelte
+++ src/components/chapter-button.svelte
@@ -8,19 +8,19 @@
 	export let light = false;
 </script>
 
-<div class=" sm:w-max shadow-lg">
+<div class=" sm:w-max shadow-lg m-4 md:m-12">
 	<a
 		id="button"
-		class="group flex flex-row items-center justify-center rounded-sm p-4 shadow-black/30 transition-all duration-500  sm:gap-6"
+		class="group flex flex-row items-center justify-center rounded-xl p-4 shadow-black/30 transition-all duration-500  sm:gap-6"
 		href={path}
 	>
-		<p class="  numeral text-5xl px-2 text-gold-400 transition-colors group-hover:text-gold-400 duration-500">
+		<p class="numeral text-5xl px-2 text-gold-400 transition-colors group-hover:text-gold-400 duration-500">
 			{numeral}
 		</p>
 		<p class=" text-2xl text-white chapter">
 			{chapter} 	
 		</p>
-		<ChevronRightIcon size="3x" class="text-gold-400  transition-colors duration-1000 group-hover:text-gold-400"/>
+		<ChevronRightIcon size="2.5x" class="text-gold-400 transition-colors duration-1000 group-hover:text-gold-400"/>
 		</a>
 </div>
 
@@ -29,7 +29,7 @@
 		/* background: linear-gradient(to left, #401221 50%, rgb(84, 24, 44) 50%); */
 		background-size: 200% 100%;
 		background-position: right bottom;
-		border: #F6C869 2px solid;
+		border: #F6C869 3px solid;
 		background: rgb(84, 24, 44);
 	}
 
@@ -51,7 +51,7 @@
   }
 
   .numeral{
-	font-size: 48px;
+	font-size: 36px;
   }
 }
 </style>
