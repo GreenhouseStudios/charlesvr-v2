69234e9dbfba2364e2642d305751d1724f51f47d
melinaskye <melinaskye@optonline.net>
2023-09-12 12:39:26 -0400

    first page redesign based on clarissa class

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,8 @@
 	import NextChapter from './../components/next-chapter.svelte';
 	import { onMount } from 'svelte';
 	import Tooltip from './../components/tooltip.svelte';
+	import ImageCard from './../components/image-card.svelte';
+
 	
 	let images = [1, 2, 3, 4, 5, 6];
 	// let definitions = [{ word: 'coronation', definition: 'the act or occasion of crowning' }];
@@ -14,7 +16,7 @@
 
 <Transition>
 	<Background src="..//charlesvrvideoblur.mp4" scrim="gold" opacity1="5" opacity2="7" class="">
-		<div class="flex flex-col items-center justify-center gap-10 bg-cover bg-center p-4 md:p-16 ">
+		<div class=" flex flex-col items-center justify-center gap-10 bg-cover bg-center p-4 md:p-16 h-[80vh]">
 			<video
 				class="fixed inset-0 -z-50 aspect-auto h-screen w-screen object-cover"
 				id="videobg"
@@ -24,11 +26,18 @@
 				loop
 				style=""
 			/>
-			<img src="..//logos/charlesvr-newduotone.png" alt="Charles VR" class="z-10 mt-32 w-5/6 " />
-			<div class="w-5/6 text-center text-3xl font-medium text-[#4D1B2C] md:text-5xl">
+			<div class="md:max-w-[80%] mt-32" >
+			<img src="..//logos/charlesvr-newduotone.png" alt="Charles VR" class="z-10  w-1/2" />
+			<div class="  text-xl font-medium text-[#4D1B2C] md:text-2xl">
 				Experience the Imperial Coronation of Holy Roman Emperor Charles V
 			</div>
-			<Card light class="md:max-w-[80%]" >
+			<ChapterButton numeral="II" chapter="The Coronation in Context" path="2" class="" />
+		</div>
+			</div>
+			</Background>
+			<Background src="..//charlesvrvideoblur.mp4" scrim="light" opacity1="9" opacity2="9" class="">
+				<div class="flex items-center justify-center gap-10 bg-cover bg-center p-4 md:p-16 ">
+			<Card light class="md:max-w-[40%]" >
 				<p class="mt-10 mb-10">
 					Travel to 1530 Bologna to attend the <Tooltip def="the act or occasion of crowning">coronation</Tooltip> mass of the Holy Roman Emperor,
 					Charles V, in this virtual reality recreation of San Petronio. The details of the coronation
@@ -36,23 +45,47 @@
 					sights and sounds of this religious and political ceremony using contemporaneous accounts,
 					artwork, and music as source material.
 				</p>
-				<br>
+</Card>
+<div class="md:max-w-[40%]">
+	<ImageCard src="static/content/rota-porfiletica-st-peter.jpg" citation="https://www.romawonder.com/wp-content/uploads/2017/07/rota-porfiletica-st-
+	peter.jpg"/>
+</div>
+</div>
+</Background>
+<Background src="..//charlesvrvideoblur.mp4" scrim="maroon" opacity1="9" opacity2="9" class="">
+	<div class=" flex  items-center justify-center gap-10 bg-cover bg-center p-4 md:p-16 ">
+		<div class="md:max-w-[40%]">
+			<ImageCard src="static/content/rota-porfiletica-st-peter.jpg" citation="https://www.romawonder.com/wp-content/uploads/2017/07/rota-porfiletica-st-
+			peter.jpg"/>
+		</div>
+		<Card class="md:max-w-[40%]" clear>
 				<p class="">
 					Experience the event from multiple perspectives — that of a member of the imperial papal
-					court, a <Tooltip def="a Roman Catholic church given ceremonial privileges">basilica</Tooltip> official, or a Bolognese citizen. Hear music performed during the
+					court, a <Tooltip def="a Roman Catholic church given ceremonial privileges" light>basilica</Tooltip> official, or a Bolognese citizen. Hear music performed during the
 					ceremony as recorded by Ensemble Origo. Call up scholarly annotations to explore the
 					layers of meaning embedded in the art, architecture, and various ceremonial objects
 					present in the scene.
 				</p>
-				<br>
+			</Card>
+				</div>
+			</Background>
+			<Background src="..//charlesvrvideoblur.mp4" scrim="light" opacity1="9" opacity2="9" class="">
+<div class=" flex flex items-center justify-center gap-10 bg-cover bg-center p-4 md:p-16">
+	<Card class="md:max-w-[40%]" light>
 				<p class="">
 					Charles V|R offers a multi-modal window into this event of world-historical importance for
 					enthusiasts, students, and scholars of art, architecture, history, music, religion, and
 					virtual reality.
 				</p>
 			</Card>
-			<ChapterButton numeral="II" chapter="The Coronation in Context" path="2" light class="" />
-			<Card light class="flex flex-col md:max-w-[80%]">
+			<div class="md:max-w-[40%]">
+				<ImageCard src="static/content/rota-porfiletica-st-peter.jpg" citation="https://www.romawonder.com/wp-content/uploads/2017/07/rota-porfiletica-st-
+				peter.jpg"/>
+			</div>
+			</div>
+			</Background>
+			<Background src="..//charlesvrvideoblur.mp4" scrim="maroon" opacity1="9" opacity2="9" class="">
+			<Card dark class="flex flex-col md:max-w-[80%]">
 				<h1 class="text-center text-3xl "><strong>Contributors</strong></h1>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium
@@ -165,73 +198,51 @@
 					</div>
 				</div>
 			</Card>
-			<Card light class="flex flex-col md:max-w-[80%]">
+		</Background>
+		<Background src="..//charlesvrvideoblur.mp4" scrim="light" opacity1="9" opacity2="9" class="">
+			<div class=" flex flex-col items-center justify-center gap-10 bg-cover bg-center p-4 md:p-16">
+<Card light class="flex flex-col md:max-w-[80%]">
 				<h1 class="text-center text-3xl"><strong>Funders</strong></h1>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium
 					possimus non officiis natus quasi totam assumenda laboriosam dolores, debitis iusto esse
 					recusandae dignissimos eos architecto alias a tempore enim.
 				</p>
-				<div class="grid grid-cols-2 gap-2 ">
+				<div class="">
 					<img
 						class="col-span-2 mx-auto"
 						src="static/logos/greenhouselogo.png"
 						alt="credits placeholder"
 					/>
+					<div class="flex items-center justify-center gap-2">
 					<img
-						class="col-span-2 mx-auto"
+						class="col-span-2 mx-auto w-1/2 h-full"
 						src="static/logos/ucsantacruzlogo.png"
 						alt="credits placeholder"
 					/>
 					<img
-						class="col-span-2 mx-auto"
-						src="static/logos/Kress_Signature_K copy.jpg"
+						class="col-span-2 mx-auto w-1/2 h-full"
+						src="static/logos/kress-logo.png"
 						alt="credits placeholder"
 					/>
+				</div>
+				<div class="flex items-center justify-center gap-2">
 					<img
-						class="col-span-2 mx-auto"
+						class="col-span-2 mx-auto w-1/2 h-full"
 						src="static/logos/Mellon_Logomark_Lockup_Black.png"
 						alt="credits placeholder"
 					/>
 					<img
-						class="col-span-2 mx-auto"
+						class="col-span-2 mx-auto w-1/2 h-full"
 						src="static/logos/OrigoLogo.png"
 						alt="credits placeholder"
 					/>
-					<img
-						class="m-5 mx-auto"
-						src="https://via.placeholder.com/300
-
-C/O https://placeholder.com/"
-						alt="credits placeholder"
-					/>
-					<img
-						class="m-5 mx-auto"
-						src="https://via.placeholder.com/300
-
-C/O https://placeholder.com/"
-						alt="credits placeholder"
-					/>
-					<img
-						class="m-5 mx-auto"
-						src="https://via.placeholder.com/300
-
-C/O https://placeholder.com/"
-						alt="credits placeholder"
-					/>
-					<img
-						class="m-5 mx-auto"
-						src="https://via.placeholder.com/300
-
-C/O https://placeholder.com/"
-						alt="credits placeholder"
-					/>
-				</div></Card
+				</div>
+				</div>
+				</Card
 			>
 			<ChapterButton numeral="II" chapter="The Coronation in Context" path="2" class="" />
-		</div>
-	</Background>
-</Transition>
-
+		</Background>
+		</Transition>
 <style>
 </style>
