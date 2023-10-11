51566456b70d0964dca465fa1fe045f335acea95
melinaskye <melinaskye@optonline.net>
2023-10-11 10:42:37 -0400

    text size larger in carsd

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -6,11 +6,12 @@
 
 <div
 	class={
-		' md:leading-9 font-normal md:px-10 px-5 py-5 md:my-12 my-4 backdrop-blur-3xl rounded-md shadow-lg h-min ' +
+		' md:leading-12 font-normal md:my-12 my-4 backdrop-blur-2xl rounded-md  h-min ' +
 		(light
-			? 'bg-[#F9F4E8] bg-opacity-90 text-[#1C0A10] md:text-[20px] text-[18px]  '
-			: 'bg-[#1C0A10] bg-opacity-80 text-[#F9F4E8] md:text-[20px] text-[18px] ') +
+			? 'bg-opacity-90 text-[#1C0A10] lg:text-[28px] md:text-[24px] text-[20px]  '
+			: 'bg-opacity-80 text-[#F9F4E8] lg:text-[28px] md:text-[24px] text-[20px] ') +
 		classes}
+
 >
 	<slot />
 </div>
@@ -19,4 +20,8 @@
 	.p-4 {
 		padding: clamp(16px, 1em, 32px);
 	}
+
 </style>
+
+<!-- bg-[#F9F4E8]
+bg-[#1C0A10] -->
