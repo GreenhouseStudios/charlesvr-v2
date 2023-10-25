09cd514aba5b012e66d43013e4028cf11b45bc6c
melinaskye <melinaskye@optonline.net>
2023-10-25 10:08:31 -0400

    changing the book button to include source

--- src/components/image-card.svelte
+++ src/components/image-card.svelte
@@ -3,25 +3,40 @@ export let src;
 export let alt = '';
 export let citation;
 import 'material-icons/iconfont/material-icons.css';
+export let light = false;
 export let classes = '';
 export { classes as class };
+export let width = '';
+
 
 </script>
 
-<div class={'flex flex-col justify-center items-center' + classes }>
-    <img {src} {alt} class="rounded-md " />
+<div class={
+	'flex flex-col items-center mx-auto p-10' +
+	(light
+		? ' text-[#1C0A10]'
+		: ' text-[#F9F4E8]') +
+	classes}
+>
+    <img {src} {alt} class="rounded-md {width}" />
+    <div class="flex items-center gap-2 justify-center hover-u">
     <span class="material-icons"><a href="{citation}">menu_book</a></span>
+    <span class=" text-[24px]">Source</span>
+</div>
+<slot />
 </div>
 
 <style>
     .material-icons{
-        color: #fec755;
-        font-size: 36px;
-        margin-bottom: 10px;
+        font-size: 24px;
     }
 
     .material-icons:hover{
         color: white;
+    }
+
+    .hover-u:hover {
+        text-decoration: underline;
     }
 
     @media screen and (max-width: 768px){
