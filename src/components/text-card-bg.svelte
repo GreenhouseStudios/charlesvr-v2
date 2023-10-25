1d82dfac4876865fccc435bb852994a27a73e435
melinaskye <melinaskye@optonline.net>
2023-10-25 10:08:55 -0400

    new component when text is side by side with the background image

---
+++ src/components/text-card-bg.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let light = false;
+	export let classes = '';
+	export { classes as class };
+
+</script>
+<div
+class={
+	'' +
+	(light
+		? ' text-[#1C0A10]  bg-[#FFE2A5] '
+		: ' text-[#F9F4E8]  bg-[#54182c] ') +
+	classes}
+>
+<slot />
+</div>
+<style>
+</style>
