d3d7fd22d6bbe8edc22003006ce826642385a2fa
melinaskye <melinaskye@optonline.net>
2023-10-25 10:06:02 -0400

    make card flippable

--- src/components/bio-card.svelte
+++ src/components/bio-card.svelte
@@ -28,7 +28,7 @@
 
 <div
 class={
-	'flex flex-col items-center md:flex-row h-full w-full' +
+	'flex flex-col items-center md:flex-row p-5' +
 	(light
 		? ' text-[#1C0A10]  bg-[#FFE2A5] '
 		: ' text-[#F9F4E8]  bg-[#54182c] ') +
@@ -41,31 +41,37 @@ class={
 				{#if flipped}
 				<div class="side flex flex-col" transition:flip>
 					<div class="p-2 text-center">
-						<strong class="text-3xl lg:text-5xl">{name}</strong>
+						<strong class="text-2xl lg:text-5xl">{name}</strong>
 						<br />
-						<em class="mb-4 text-2xl md:mb-8 lg:text-4xl">{years}</em>
-						<div class="space-y-4 text-lg md:text-[20px] md:leading-12 lg:space-y-8">
+						<em class="mb-4 text-xl md:mb-8 lg:text-4xl">{years}</em>
+						<div class="space-y-4 text-lg text-[18px] md:leading-12 lg:space-y-8">
 							<slot />
 						</div>
-						<img src="../static/content/flip-icon-{color}.png" alt="flip button" class="flip mx-auto">
+						<!-- <img src="../static/content/flip-icon-{color}.png" alt="flip button" class="flip mx-auto"> -->
 					</div>
 				</div>
 				{:else}
-				<div class="side back" transition:flip>
-					<img {src} {alt} class="rounded-md bioimg" />
-					<img src="../static/content/flip-icon-{color}.png" alt="flip button" class="flip">
+				<div class="side" transition:flip>
+					<img {src} {alt} class="rounded-md bioimg h-full" />
+					<!-- <img src="../static/content/flip-icon-{color}.png" alt="flip button" class="flip"> -->
 				</div>
 				{/if}
 			</div>
 		</div>
+		<img src="../static/content/flip-icon-{color}.png" alt="flip button" class="flip m-1 h-[25px] w-[25px] md:h-[35px] md:w-[35px]" on:click={() => flipped = !flipped}>
+
 	</div>
 </div>
 
 <style>
 
 	.flip {
-		width: 35px;
-		height: 35px;
+		transition: all 0.3s ease-in-out;
+		cursor: pointer;
+	}
+
+	.flip:hover {
+		transform: scale(1.3)
 	}
 
 	.black-hidden {
@@ -105,21 +111,25 @@ class={
 			object-fit: cover;
 			padding: 10px;
 			width: 400px; 
-            height: 250px;
+            height: 300px;
 			object-position: 80% 20%;;
+		}
+		.flip{
+			position: relative;
+			top: 20px;
 		}
 	}
 	.card-container {
 		position: relative;
-		height: 100%;
-		width: 100%;
+		height: 450px;
+		width: 300px;
+		margin: 10px;
 	}
 	
 	.card {
 		width: 100%;
 		height: 100%;
 		position: absolute;
-
 	}
 	
 	.side {
@@ -127,9 +137,9 @@ class={
 		height: 100%;
 		width: 100%;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex-direction: column;
-	}
-	
+		}
+
 </style>
