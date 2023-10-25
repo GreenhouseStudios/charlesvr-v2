710d598fb1bcf90dbc6c00b90f5149211f5380fc
melinaskye <melinaskye@optonline.net>
2023-10-25 10:09:37 -0400

    new component for when text is on a solid background

---
+++ src/components/text-card.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let light = false;
+	export let classes = '';
+	export { classes as class };
+
+</script>
+<div class={ 'h-full flex items-center ' + 	(light
+? '  bg-[#FFE2A5] '
+: ' bg-[#54182c] ') }>
+<div
+class={
+	'' +
+	(light
+		? ' text-[#1C0A10]  bg-[#FFE2A5] '
+		: ' text-[#F9F4E8]  bg-[#54182c] ') +
+	classes}
+>
+<slot />
+</div>
+</div>
+
+<style>
+</style>
