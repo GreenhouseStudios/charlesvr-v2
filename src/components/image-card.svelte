1e81a028a8f796002014834066f1fbd82af2e9c0
melinaskye <melinaskye@optonline.net>
2023-10-31 12:18:35 -0400

    making source and book underline

--- src/components/image-card.svelte
+++ src/components/image-card.svelte
@@ -19,9 +19,16 @@ export let width = '';
 	classes}
 >
     <img {src} {alt} class="rounded-md {width}" />
-    <div class="flex items-center gap-2 justify-center hover-u">
-    <span class="material-icons"><a href="{citation}">menu_book</a></span>
+    <div
+    class='flex flex-col items-center justify-center source'
+>
+<a href="{citation}">
+<div class="flex items-center gap-2">
+    <span class="material-icons">menu_book</span>
     <span class=" text-[24px]">Source</span>
+</div>
+    <hr class={(light ? 'horizontal-line-black' : 'horizontal-line-white')}>
+</a>
 </div>
 <slot />
 </div>
@@ -30,18 +37,33 @@ export let width = '';
     .material-icons{
         font-size: 24px;
     }
-
-    .material-icons:hover{
-        color: white;
-    }
-
-    .hover-u:hover {
-        text-decoration: underline;
-    }
-
     @media screen and (max-width: 768px){
         .material-icons{
         font-size: 24px;
     }  
+    }
+    .horizontal-line-black {
+        border: 1px solid black ;
+        width: 100px;
+        display:none
+    }
+
+    .horizontal-line-white {
+        border: 1px solid white ;
+        width: 100px;
+        display:none;
+        bottom: 5px
+    }
+
+    .source {
+        position: relative;
+        cursor: pointer;
+
+    }
+
+    .source:hover hr {
+        display: block;
+        position: absolute;
+        bottom: 1px;
     }
 </style>
