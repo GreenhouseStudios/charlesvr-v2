ec07be44261d973f9009e7b5cc2b0456d655fdcc
melinaskye <melinaskye@optonline.net>
2023-10-31 12:18:22 -0400

    making source and book underline

--- src/components/image-card-color.svelte
+++ src/components/image-card-color.svelte
@@ -6,45 +6,56 @@
 	export { classes as class };
 	import 'material-icons/iconfont/material-icons.css';
 	export let citation;
-
 </script>
 
 <div
-class={
-	'flex flex-col items-center md:flex-row p-5' +
-	(light
-		? ' text-[#1C0A10]  bg-[#FFE2A5] '
-		: ' text-[#F9F4E8]  bg-[#54182c] ') +
-	classes}
-
+	class={'flex flex-col items-center md:flex-row p-5' +
+		(light ? ' text-[#1C0A10]  bg-[rgb(255,226,165)]  ' : ' text-[rgb(249,244,232)]  bg-[#54182c]  ') +
+		classes}
 >
 	<div class=" bioimg flex w-1/2 flex-col items-center justify-center break-words">
-					<img {src} {alt} class="rounded-md bioimg" />
-					<span class="material-icons"><a href="{citation}">menu_book</a>Source</span>
+		<img {src} {alt} class="rounded-md" />
+		<div
+		class='flex flex-col items-center justify-center source'
+	>
+	<a href="{citation}">
+	<div class="flex items-center gap-2">
+		<span class="material-icons">menu_book</span>
+		<span class=" text-[24px]">Source</span>
+	</div>
+		<hr class={(light ? 'horizontal-line-black' : 'horizontal-line-white')}>
+	</a>
+	</div>
 	</div>
 </div>
 
 <style>
 	.material-icons {
-		font-size: 36px;
+		font-size: 24px;
 	}
 
+	.horizontal-line-black {
+        border: 1px solid black ;
+        width: 100px;
+        display:none
+    }
 
-	.material-icons:hover {
-		text-decoration: underline;
-	}
+    .horizontal-line-white {
+        border: 1px solid white ;
+        width: 100px;
+        display:none;
+        bottom: 5px
+    }
 
-	@media screen and (max-width: 768px) {
-		.material-icons {
-			font-size: 24px;
-		}
-		.bioimg{
-			object-fit: cover;
-			padding: 10px;
-			width: 400px; 
-            height: 250px;
-			object-position: 80% 20%;;
-		}
-	}
+    .source {
+        position: relative;
+        cursor: pointer;
 
+    }
+
+    .source:hover hr {
+        display: block;
+        position: absolute;
+        bottom: 1px;
+    }
 </style>
