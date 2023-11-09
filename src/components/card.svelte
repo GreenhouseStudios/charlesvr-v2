3378023fa3737f47861fdb308e9ef8411fef829d
melinaskye <melinaskye@optonline.net>
2023-11-09 12:07:11 -0500

    changing card formatting

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -6,7 +6,7 @@
 
 <div
 	class={
-		'font-normal md:my-12 my-4 backdrop-blur-2xl rounded-md  h-min m-4 p-4 md:m-16 md:p-16 leading-relaxed ' +
+		'font-normal md:my-12 my-4 backdrop-blur-2xl rounded-md  h-min m-6 p-4 md:m-16 md:p-16 leading-relaxed ' +
 		(light
 			? 'bg-opacity-90 text-[#1C0A10] md:text-[24px] text-[20px]  '
 			: 'bg-opacity-80 text-[#F9F4E8] md:text-[24px] text-[20px] ') +
