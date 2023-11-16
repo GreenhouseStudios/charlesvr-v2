427c55b1451c4ee6df009379ca8c7b7c2660faeb
melinaskye <melinaskye@optonline.net>
2023-11-16 10:14:01 -0500

    adding margin to cards

--- src/components/text-card.svelte
+++ src/components/text-card.svelte
@@ -9,7 +9,7 @@
 : ' bg-[#54182c] ') }>
 <div
 class={
-	'font-normal backdrop-blur-2xl rounded-md  h-min m-6 pb-12 pt-4 pl-4 pr-4 p-4 md:m-16 md:p-16 leading-relaxed  text-[18px] md:text-[20px] lg:text-[22px]' +
+	'font-normal backdrop-blur-2xl rounded-md  h-min m-2 pb-12 pt-4 pl-4 pr-4 p-4 md:m-16 md:p-16 leading-relaxed  text-[18px] md:text-[20px] lg:text-[22px]' +
 	(light
 		? ' text-[#1C0A10]  bg-[#FFE2A5] text-[18px] md:text-[20px] lg:text-[22px] '
 		: ' text-[#F9F4E8]  bg-[#54182c] text-[18px] md:text-[20px] lg:text-[22px]') +
